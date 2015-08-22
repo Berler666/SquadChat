8174b8afa3dbcca9ef9f712e2a218bda3f2ad066
Berler666 <destroyed76@hotmail.com>
2015-08-22 16:41:16 +1000

    dlls

---
+++ Assets/Scripts/mobileKeyboard.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mobileKeyboard : MonoBehaviour {
+
+	public void OpenKeyboard()
+	{
+		TouchScreenKeyboard.Open("", TouchScreenKeyboardType.ASCIICapable);
+	}
+}
